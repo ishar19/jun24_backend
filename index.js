cf9415f6a2c414642efe79171ad9295b53768b7b
ishar19 <isharjain19@gmail.com>
2024-11-02 22:02:35 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const env = require("dotenv");
+env.config();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port 3000");
+});
+
+
+
