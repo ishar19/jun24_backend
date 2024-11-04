fe37c8fb6b830fd0c1672b4cdb54b2c37953097f
ishar19 <isharjain19@gmail.com>
2024-11-04 22:28:21 +0530

    feat: added auth

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
 const env = require("dotenv");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
 env.config();
 const PORT = process.env.PORT || 3000;
-
+const MONGO_URI = process.env.MONGO_URI;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
-
+app.use("/api/user", userRoutes);
 app.listen(PORT, () => {
     console.log("Server is running on port 3000");
+    mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log("Connected to MongoDB");
+    }).catch((err) => {
+        console.log(err);
+    });
 });
 
 
