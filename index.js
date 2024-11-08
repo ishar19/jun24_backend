8418421de381b0e24eaa3876e8fb463f0ced4599
ishar19 <isharjain19@gmail.com>
2024-11-08 22:24:00 +0530

    added cors

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const env = require("dotenv");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
+const cors = require("cors");
+app.use(cors(
+    // {
+    //     origin: "*",
+    // }
+));
 env.config();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
